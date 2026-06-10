75a9921da7769350640ebd14e87885243a2a9631
Matin <132759816+No22net@users.noreply.github.com>
2026-06-10 21:47:15 +0330

    Delete .next directory

--- .next/dev/static/chunks/app_page_tsx_0zr2k9a._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_136f60g._.js","static/chunks/node_modules_0al5vh_._.js"],
-    source: "dynamic"
-});
